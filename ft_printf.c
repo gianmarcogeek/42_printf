aa92cb02c5dd7b8bcd31053ecba24fc685f18f8c
Gianmarco Pusceddu <gpuscedd@e4r1p1.42roma.it>
2024-01-26 18:11:45 +0100

    added %x %X %p

--- ft_printf.c
+++ ft_printf.c
@@ -6,14 +6,13 @@
 /*   By: gpuscedd <gpuscedd@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 16:03:37 by gpuscedd          #+#    #+#             */
-/*   Updated: 2024/01/25 20:42:23 by gpuscedd         ###   ########.fr       */
+/*   Updated: 2024/01/26 18:08:50 by gpuscedd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 /* TODO
-	- puthex - putnbr_base
 	- putptr - capire meglio
 */
 
@@ -30,12 +29,15 @@ int	ft_format(va_list args, const char format)
 			len += ft_putstr_fd(va_arg(args, char *), 1);
 		else if (format == 'd' || format == 'i')
 			len += ft_putnbr_fd(va_arg(args, int), 1);
-		/*else if (format == 'p')
-			len += ft_putptr(va_arg(args, &void *));*/
+		else if (format == 'p')
+			len += ft_putstr_fd("0x", 1);
+			len += ft_putnbr_base(va_arg(args, int), "0123456789abcdef");
 		else if (format == 'u')
 			len += ft_putnbr_fd(va_arg(args, unsigned int), 1);
-		/*else if (format == 'x' || format == 'X')
-			len += ft_putnbr_base(va_arg(args, unsigned int));*/
+		else if (format == 'X')
+			len += ft_putnbr_base(va_arg(args, int), "0123456789ABCDEF");
+		else if (format == 'x')
+			len += ft_putnbr_base(va_arg(args, int), "0123456789abcdef");
 		return (len);
 }
 
@@ -67,6 +69,9 @@ int	ft_printf(const char *str, ...)
 
 int main()
 {
-	int len = ft_printf("|Test, %% %c %s %d %u|", 'x', "gpuscedd", 2147483648, 2147483648);
+	int len = ft_printf("|Test, %% %c %s %d %u %x |", 'x', "gpuscedd", 2147483647, 2147483, 53423423);
 	ft_printf("	[%d]", len);
+
+	int len2 = printf("|Test, %% %c %s %d %u %x |", 'x', "gpuscedd", 2147483647, 2147483, 53423423);
+	printf("	[%d]", len2);
 }
